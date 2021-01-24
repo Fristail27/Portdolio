c927ad6e6b190833e86db2727d7fcee4deba9fb0
Fristail27 <fristail27@gmail.com>
2021-01-24 18:26:36 +0300

    portfolio/template

--- src/common/button/Button.tsx
+++ src/common/button/Button.tsx
@@ -2,14 +2,24 @@ import React from "react";
 import s from "./Button.module.css"
 
 type ButtonPropsType = {
+<<<<<<< HEAD
     name:string;
     onClick?: ()=> void
+=======
+    name:string
+>>>>>>> 2280c26... portfolio/template
 }
 
 const Button:React.FC<ButtonPropsType> = (props) => {
     return (
+<<<<<<< HEAD
         <button onClick={props.onClick} className={s.button}>{props.name}</button>
     )
 };
+=======
+        <button className={s.button}>{props.name}</button>
+    )
+}
+>>>>>>> 2280c26... portfolio/template
 
 export default Button
